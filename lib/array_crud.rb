86ca5235cd90f02d0e5e315c9db5d01ba68d350f
Emily Harber <emily.a.harber@gmail.com>
2019-08-08 01:58:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  my_array = ["this","that","other","another"];
-  my_array << "one more"
+ array << element
 end
 
 def add_element_to_start_of_array(array, element)
